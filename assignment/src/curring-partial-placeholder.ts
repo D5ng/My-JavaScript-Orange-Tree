7eb1e05363440ed47e0df39cf2f1f3c6fbdf7491
d5ng <dong5ffice@gmail.com>
2025-02-10 23:30:24 +0900

    ✨ Feat: Curring Partial Placeholder 구현

---
+++ assignment/src/curring-partial-placeholder.ts
@@ -0,0 +1,41 @@
+// const  join = (a, b, c) => {
+//     return `${a}_${b}_${c}`
+//  }
+//  const curriedJoin = curry(join)
+//  const _ = curry.placeholder
+//  curriedJoin(1, 2, 3) // '1_2_3'
+//  curriedJoin(_, 2)(1, 3) // '1_2_3'
+//  curriedJoin(_, _, _)(1)(_, 3)(2) // '1_2_3'
+
+type Func = (...args: any[]) => any
+
+const curry = (function () {
+  const curry = (func: Func) => {
+    const curried = (...args: any[]) => {
+      const hasPlaceholder = args.includes(curry.placeholder)
+
+      if (func.length === args.length && !hasPlaceholder) {
+        return func(...args)
+      }
+
+      return (...nextArgs: any[]) => {
+        const mergeArgs = args.map((arg) => (arg === curry.placeholder && nextArgs.length ? nextArgs.shift() : arg))
+        return curried.call(null, ...mergeArgs, ...nextArgs)
+      }
+    }
+
+    return curried
+  }
+
+  curry.placeholder = "placeholder"
+
+  return curry
+})()
+
+const join = (a: string, b: string, c: string) => `${a}_${b}_${c}`
+const curriedJoin = curry(join)
+const _ = curry.placeholder
+
+console.log(curriedJoin(1, 2, 3))
+console.log(curriedJoin(_, 2)(1, 3))
+console.log(curriedJoin(_, _, _)(1)(_, 3)(2))
