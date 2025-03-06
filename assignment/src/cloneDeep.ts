a950d25316d22e07a3cc3be27b6892c50bcee79c
d5ng <dong5ffice@gmail.com>
2025-03-06 22:42:14 +0900

    ✨ Feat: 객체 깊은 복사 구현

--- assignment/src/cloneDeep.ts
+++ assignment/src/cloneDeep.ts
@@ -1,11 +1,23 @@
 // Todo: 깊은 복사 구현하기
 // Todo: ✅ 1단계 객체만 복사하기. (깊은 복사X)
+// Todo: ✅ 2단계 객체만 복사하기. (깊은 복사)
 function cloneDeep<T extends Record<string, any>>(target: T): T
 function cloneDeep<T>(target: T) {
+  // 원시타입이 아니라면 => target이 객체가 아니거나, 널이라면
+  // null === object
+  if (target === null) {
+    return null
+  }
+
+  // null과 object가 아니라면!
+  if (typeof target !== "object") {
+    return target
+  }
+
   const clonedValue: Record<string, any> = {}
 
   for (const [key, value] of Object.entries(target as Record<string, any>)) {
-    clonedValue[key] = value
+    clonedValue[key] = cloneDeep(value)
   }
 
   return clonedValue as T
@@ -14,10 +26,17 @@ function cloneDeep<T>(target: T) {
 const user = {
   name: "김철수",
   age: 32,
-  address: "서울특별시 강남구 테헤란로 123",
   gender: "Male",
+  address: {
+    street: "서울특별시 강남구 테헤란로 123",
+    city: "서울",
+    state: "강남구",
+    country: "대한민국",
+    zipCode: "06164",
+  },
 }
 
 const cloneUser = cloneDeep(user)
 
 console.log(cloneUser === user)
+console.log(cloneUser.address === user.address)
