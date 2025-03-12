666bf3e357b5bb8fcff2bea0ddaa621047a0bfab
d5ng <dong5ffice@gmail.com>
2025-03-12 22:28:55 +0900

    ✨ Feat: Chunk 함수 구현

---
+++ assignment/src/chunk.ts
@@ -0,0 +1,27 @@
+/**
+ * @example
+ * // chunk
+ * chunk([1, 2, 3, 4, 5], 2) => [[1, 2],[3, 4],[5]]
+ * chunk([1, 2, 3], 1) => [[1],[2],[3]]
+ * chunk([], 5) => []
+ */
+
+export function chunk(arr: any[], size: number) {
+  if (size < 1) {
+    console.warn(`두번째 매개변수(size)에는 1이상의 값을 넣어주어야 합니다.`)
+    return []
+  }
+
+  if (arr.length === 0) {
+    return []
+  }
+
+  const chunks = []
+
+  for (let i = 0; i < arr.length; i += size) {
+    const chunkData = arr.slice(i, i + size)
+    chunks.push(chunkData)
+  }
+
+  return chunks
+}
