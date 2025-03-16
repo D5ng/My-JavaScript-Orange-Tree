cb19ccdb74c142d67a3a870298dbfa4cb038f5d1
d5ng <dong5ffice@gmail.com>
2025-03-16 19:14:58 +0900

    ♻️ Refactor: change size => depth

--- assignment/src/flat.ts
+++ assignment/src/flat.ts
@@ -9,23 +9,23 @@
  * // [1, 2, 3, 4]
  */
 
-export function flat<T>(arr: T[], size: number = 1) {
+export function flat<T>(arr: T[], depth: number = 1) {
   let flatten: any[] = []
   // Todo: 배열의 Empty 슬롯이 있다면 없애버리기
   const notEmptyArray = arr.filter(() => true)
 
-  // Todo: size가 0보다 작다면~
-  if (size < 0) {
-    throw new Error(`size의 값은 1이상이어야 해요.`)
+  // Todo: depth가 0보다 작다면~
+  if (depth < 0) {
+    throw new Error(`depth의 값은 1이상이어야 해요.`)
   }
 
-  // Todo: size의 값이 0이라면
-  if (size === 0) {
+  // Todo: depth의 값이 0이라면
+  if (depth === 0) {
     return arr
   }
 
   // Todo: 더 이상 배열을 평탄화할 수 없을 때
-  const isFlattened = notEmptyArray.every((value) => !Array.isArray(value)) && size > 0
+  const isFlattened = notEmptyArray.every((value) => !Array.isArray(value)) && depth > 0
   if (isFlattened) {
     return notEmptyArray
   }
@@ -35,8 +35,8 @@ export function flat<T>(arr: T[], size: number = 1) {
     flatten = flatten.concat(value)
   }
 
-  // Todo: size가 없어도 기본 값으로 1로 진행하도록 되어야함.
-  flatten = flat(flatten, size - 1)
+  // Todo: depth가 없어도 기본 값으로 1로 진행하도록 되어야함.
+  flatten = flat(flatten, depth - 1)
 
   return flatten
 }
