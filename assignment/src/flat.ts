990b6f13c9fde9e9f3c619fedd67b2d0ff19b487
d5ng <dong5ffice@gmail.com>
2025-03-16 19:11:32 +0900

    ✨ Feat: Array empty 처리

--- assignment/src/flat.ts
+++ assignment/src/flat.ts
@@ -11,6 +11,8 @@
 
 export function flat<T>(arr: T[], size: number = 1) {
   let flatten: any[] = []
+  // Todo: 배열의 Empty 슬롯이 있다면 없애버리기
+  const notEmptyArray = arr.filter(() => true)
 
   // Todo: size가 0보다 작다면~
   if (size < 0) {
@@ -23,13 +25,13 @@ export function flat<T>(arr: T[], size: number = 1) {
   }
 
   // Todo: 더 이상 배열을 평탄화할 수 없을 때
-  const isFlattened = arr.every((value) => !Array.isArray(value)) && size > 0
+  const isFlattened = notEmptyArray.every((value) => !Array.isArray(value)) && size > 0
   if (isFlattened) {
-    return arr
+    return notEmptyArray
   }
 
   // Todo: 각 요소를 순회하면서 배열을 평탄화 작업해야함.
-  for (const value of arr) {
+  for (const value of notEmptyArray) {
     flatten = flatten.concat(value)
   }
 
@@ -38,6 +40,3 @@ export function flat<T>(arr: T[], size: number = 1) {
 
   return flatten
 }
-
-const arr = [1, [2], [3, [4]]]
-flat(arr, 10)
