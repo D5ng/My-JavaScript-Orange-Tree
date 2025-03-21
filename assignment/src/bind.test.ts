c015341903098959cf79ec16b76b9253566078e2
d5ng <dong5ffice@gmail.com>
2025-03-22 00:04:51 +0900

    ✅ Test: 테스트 코드 추라 (Binding)

--- assignment/src/bind.test.ts
+++ assignment/src/bind.test.ts
@@ -1,6 +1,17 @@
 import { bind } from "./bind"
 
 describe("bind 함수 테스트", () => {
+  test("bind Basic", () => {
+    const users = { name: "d5ng" }
+
+    function getName(this: { name: string }) {
+      return `내 이름은 ${this.name}이에요!`
+    }
+
+    const bindingUser = bind(getName, users)
+    expect(bindingUser()).toBe("내 이름은 d5ng이에요!")
+  })
+
   test("bind Partial", () => {
     function greet(this: { name: string }, greeting: string, punctuation: string) {
       return `${greeting}, ${this.name}${punctuation}`
@@ -13,8 +24,6 @@ describe("bind 함수 테스트", () => {
   })
 
   test("생성자 함수 테스트", () => {
-    // 즉 생성자 함수를 사용할 때에도 this binding이 되야함
-    // 어떻게?
     function Person(this: Record<string, any>, name: string) {
       this.name = name
     }
